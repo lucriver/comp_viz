3eae5eec10c957019b3a525b72e991ab3e4337f9
lucriver <lucrivDEV@gmail.com>
2022-11-20 13:15:41 -0600

    Added resolution value to available networks.

--- comp_viz/config.py
+++ comp_viz/config.py
@@ -5,11 +5,11 @@ class Models:
   tasks = ["Object Detection"]
 
 class ObjectDetection:
-  networks = [
-    "yolo3_mobilenet1.0_coco",
-    "center_net_resnet101_v1b_dcnv2_coco",
-    "ssd_512_resnet50_v1_coco",
-    "yolo3_darknet53_coco",
-    "faster_rcnn_fpn_resnest50_v1b_coco",
-    "faster_rcnn_fpn_syncbn_resnest269_coco"
-    ]
+  networks = {
+      "yolo3_mobilenet1.0_coco": { "resolution": 320 },
+      "yolo3_darknet53_coco": { "resolution": 416 },
+      "ssd_512_resnet50_v1_coco": { "resolution": 512 },
+      "center_net_resnet101_v1b_dcnv2_coco": { "resolution": 512 },
+      "faster_rcnn_fpn_resnet50_v1b_coco": { "resolution": 600 },
+      "faster_rcnn_fpn_syncbn_resnest269_coco": { "resolution": 600 }
+  }
