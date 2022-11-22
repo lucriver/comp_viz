12aef9f74fd70b806b2b13046586bfbf29a7fbab
lucrivdev <lucrivdev@gmail.com>
2022-11-22 10:46:33 -0600

    updated version to 1.0.0 and identical resolutions among networks.

--- comp_viz/config.py
+++ comp_viz/config.py
@@ -1,15 +1,15 @@
 class CompViz:
-  version = "0.0.1"
+  version = "1.0.0"
 
 class Models:
   tasks = ["Object Detection"]
 
 class ObjectDetection:
   networks = {
-      "yolo3_mobilenet1.0_coco": { "resolution": 320 },
+      "yolo3_mobilenet1.0_coco": { "resolution": 416 },
       "yolo3_darknet53_coco": { "resolution": 416 },
-      "ssd_512_resnet50_v1_coco": { "resolution": 512 },
-      "center_net_resnet101_v1b_dcnv2_coco": { "resolution": 512 },
-      "faster_rcnn_fpn_resnet50_v1b_coco": { "resolution": 600 },
-      "faster_rcnn_fpn_syncbn_resnest269_coco": { "resolution": 600 }
+      "ssd_512_resnet50_v1_coco": { "resolution": 416 },
+      "center_net_resnet101_v1b_dcnv2_coco": { "resolution": 416 },
+      "faster_rcnn_fpn_resnet50_v1b_coco": { "resolution": 416 },
+      "faster_rcnn_fpn_syncbn_resnest269_coco": { "resolution": 416 }
   }
