31c12c4f66e473c0be8b2e63db2a30eef114eb2d
lucrivdev <lucrivdev@gmail.com>
2022-12-05 15:22:06 -0600

    Updated network order to properly reflect network speed in descending order.

--- comp_viz/config.py
+++ comp_viz/config.py
@@ -22,8 +22,8 @@ class ObjectDetection(CompViz):
   """
   networks = {
       "yolo3_mobilenet1.0_coco": { "resolution": 416 },
-      "yolo3_darknet53_coco": { "resolution": 416 },
       "ssd_512_resnet50_v1_coco": { "resolution": 416 },
+      "yolo3_darknet53_coco": { "resolution": 416 },
       "center_net_resnet101_v1b_dcnv2_coco": { "resolution": 416 },
       "faster_rcnn_fpn_resnet50_v1b_coco": { "resolution": 416 },
       "faster_rcnn_fpn_syncbn_resnest269_coco": { "resolution": 416 }
